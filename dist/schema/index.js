d3fe71c1ffaa10b606edc16cc30bb033e8e6da49
Ramos Hassan <ramoshassan@Ramoss-MacBook-Pro.local>
2024-06-09 09:51:41 +0300

    added ids on the data

--- dist/schema/index.js
+++ dist/schema/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.typeDefs = void 0;
 exports.typeDefs = `#graphql
   type Book {
+    id:Int  
     title: String
     author: String
     coverPhotoURL: String
